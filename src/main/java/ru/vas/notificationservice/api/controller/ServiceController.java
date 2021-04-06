e381d9076f4830d805497dcb7813588001c63c5a
carlosray <carlos.ray@mail.ru>
2021-04-06 18:16:30 +0300

    @RequestBody

--- src/main/java/ru/vas/notificationservice/api/controller/ServiceController.java
+++ src/main/java/ru/vas/notificationservice/api/controller/ServiceController.java
@@ -3,6 +3,7 @@ package ru.vas.notificationservice.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vas.notificationservice.dto.SubscriptionDTO;
@@ -17,7 +18,7 @@ public class ServiceController {
     private final SubscriptionService subscriptionService;
 
     @PostMapping("statuses")
-    public ResponseEntity<Set<SubscriptionDTO>> checkStatuses(Set<SubscriptionDTO> subs) {
+    public ResponseEntity<Set<SubscriptionDTO>> checkStatuses(@RequestBody Set<SubscriptionDTO> subs) {
         return ResponseEntity.ok(subscriptionService.setStatuses(subs));
     }
 }
